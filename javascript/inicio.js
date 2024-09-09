34be71d3809e51b3bb5eecf4074bf8f1ba79db1a
Aconselhamento Psicológico Aprofundado <00001075997070sp@al.educacao.sp.gov.br>
2024-09-09 17:15:22 -0300

    site

---
+++ javascript/inicio.js
@@ -0,0 +1,15 @@
+function searchContent() {
+  const input = document.getElementById("searchBar").value.toLowerCase();
+  const blocks = document.getElementsByClassName("block");
+
+  for (let i = 0; i < blocks.length; i++) {
+    const block = blocks[i];
+    const name = block.getAttribute("data-name");
+
+    if (name.includes(input)) {
+      block.style.display = "";
+    } else {
+      block.style.display = "none";
+    }
+  }
+}
